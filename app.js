46b7ca818a0452abb94285e27f6e1322653e105f
Yunus Ergül <56773451+yunusergul@users.noreply.github.com>
2021-12-23 09:38:28 +0300

    ptkTask | NodeJs Task 8

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const express = require("express");
+const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
-    res.end();
+  res.render('index')
+});
+
+app.get('/post', (req, res) => {
+  res.render('post')
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
 });
 
 const port = 3000;
